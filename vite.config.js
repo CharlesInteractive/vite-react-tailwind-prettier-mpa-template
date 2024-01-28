e2298b8c376f616dd140f032403e1ed92d01dc56
Charles Carroll <zarlss43@gmail.com>
2024-01-28 15:59:36 -0500

    Fix node errors
    Fix node errors due to commonjs variables not being available

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
